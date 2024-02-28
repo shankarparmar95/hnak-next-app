ca7b422fe0f4271db62238830760bd5eb4342060
Shankar Parmar <shankar.parmar@krishtechnolabs.com>
2024-02-28 18:46:46 +0530

    Created next js product

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
-import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>Hello, Shankar</main>;
+  return (
+    <main>
+      <h1>Hello, Shankar</h1>
+      <div>
+        <Link href="/menna-o-fena/prayer-needs-mof/subha-mof">
+          Go to Product Listing Page
+        </Link>{" "}
+      </div>
+      <div>
+        <Link href="/mma-lava-beads-jute-tassel">
+          Go to Product Detail Page
+        </Link>
+      </div>
+    </main>
+  );
 }
